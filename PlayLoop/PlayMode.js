0c52c2a9c6837602ff9c0a0b7d314855b939c345
l0bster2 <147654370+l0bster2@users.noreply.github.com>
2023-11-02 20:59:24 +0100

    Add files via upload

---
+++ PlayLoop/PlayMode.js
@@ -0,0 +1,125 @@
+class PlayMode {
+
+    static staticConstructor(player, tilemapHandler) {
+        this.player = player;
+        this.tilemapHandler = tilemapHandler;
+        this.deathPauseFrames = 24;
+        this.currentPauseFrames = 0;
+        this.animateToNextLevel = false;
+        this.customExit;
+    }
+
+    static runStartScreenLogic() {
+        this.player.x = 0;
+        this.player.y = 0;
+        //we need this check, because on mobile, the music will only start playing after a user interaction. A user interaction is a touch start and touch end
+        if ((Controller.enter && !Controller.mobileEnter) || Controller.mobileEnterReleased) {
+            Controller.enterReleased = false;
+            this.startGame();
+            SoundHandler.guiSelect.stopAndPlay();
+        }
+    }
+
+    static startGame() {
+        GameStatistics.resetPermanentObjects();
+        tileMapHandler.currentLevel = 1;
+        if (typeof LevelNavigationHandler === 'function') {
+            LevelNavigationHandler.updateLevel();
+        }
+        else {
+            if (SoundHandler?.song?.sound?.src && !WorldDataHandler.insideTool) {
+                SoundHandler.song.stopAndPlay();
+            }
+            tileMapHandler.resetLevel(tileMapHandler.currentLevel);
+        }
+        GameStatistics.resetPlayerStatistics();
+        GameStatistics.startTimer();
+    }
+
+    static runPlayLogic() {
+        const { player } = this;
+
+        PauseHandler.checkPause();
+
+        if (!player.death && this.currentPauseFrames === 0 && !DialogueHandler.active && !PauseHandler.paused) {
+            this.updateGeneralFrameCounter();
+            var walking = WalkHandler.walkHandler();
+            FallHandler.coyoteFrameHandler();
+            JumpHandler.wallJumpAllowedHandler();
+            player.forcedJumpSpeed !== 0 && JumpHandler.performJump(player.forcedJumpSpeed, player.maxJumpFrames + player.extraTrampolineJumpFrames);
+            AlternativeActionHandler.dashHandler();
+            Controller.alternativeActionButtonReleased = Controller.alternativeActionButton ? false : true;
+            JumpHandler.jumpHandler();
+            if (!player.dashing) {
+                //const runButtonReleased = WalkHandler.isRunButtonReleased();
+                !walking && WalkHandler.slowDownWalkSpeed();
+                FallHandler.fallHandler();
+            }
+            FallHandler.correctMaxYSpeed();
+            player.resetTemporaryAttributes();
+            CharacterCollision.checkCollisionsWithWorld(player, true);
+        }
+    }
+
+    static updateGeneralFrameCounter() {
+        tileMapHandler.currentGeneralFrameCounter++;
+        if (tileMapHandler.currentGeneralFrameCounter > tileMapHandler.generalFrameCounterMax) {
+            tileMapHandler.currentGeneralFrameCounter = 0;
+        }
+    }
+
+    static pauseFramesHandler() {
+        if (PauseHandler.paused) {
+            PauseHandler.handlePause();
+        }
+        else if (this.currentPauseFrames > 0) {
+            this.currentPauseFrames--
+            if (player.death) {
+                if (this.currentPauseFrames === this.deathPauseFrames / 2) {
+                    this.player.resetPosition(true);
+                    this.player.invisible = false;
+                    this.player.death = false;
+                    tileMapHandler.resetDynamicObjects();
+                }
+            }
+            if (this.animateToNextLevel) {
+                TransitionAnimationHandler.displayTransition();
+            }
+        }
+        else {
+            if (this.animateToNextLevel) {
+                this.animateToNextLevel = false;
+            }
+            this.currentPauseFrames = 0;
+        }
+    }
+
+    static playerDeath() {
+        if(this.animateToNextLevel) {
+            return null;
+        }
+        SoundHandler.hit.stopAndPlay();
+        this.currentPauseFrames = this.deathPauseFrames;
+        this.player.death = true;
+        const direction = AnimationHelper.facingDirections.bottom;
+        for (var i = 0; i < 7; i++) {
+            SFXHandler.createSFX(player.x, player.y, 2, direction, MathHelpers.getSometimesNegativeRandomNumber(0, 2, false), MathHelpers.getSometimesNegativeRandomNumber(0, 2, false), true, 22);
+        }
+        this.player.resetAttributes();
+        GameStatistics.deathCounter++;
+        tileMapHandler.currentGeneralFrameCounter = 0;
+    }
+
+    static checkActiveCheckPoints() {
+        let checkPointPos = null;
+        this.tilemapHandler && this.tilemapHandler.levelObjects.forEach(levelObject => {
+            if (levelObject.type === ObjectTypes.CHECKPOINT && levelObject?.active) {
+                checkPointPos = {
+                    x: levelObject.initialX * this.tilemapHandler.tileSize,
+                    y: levelObject.initialY * this.tilemapHandler.tileSize
+                }
+            }
+        });
+        return checkPointPos;
+    }
+}
---
+++ PlayLoop/WalkHandler.js
@@ -0,0 +1,82 @@
+class WalkHandler extends PlayMode {
+
+    static walkHandler() {
+        const { player } = this;
+        var walking = false;
+        if (!player.dashing && !player.fixedSpeed) {
+            //const newMaxSpeed = Controller.alternativeActionButton && player.runChecked ? player.maxSpeed * 1.65 : player.maxSpeed;
+            const newMaxSpeed = player.currentMaxSpeed;
+            //const newMaxSpeed = Controller.alternativeActionButton ? player.maxSpeed * 1.85 : player.maxSpeed;
+
+            if ((Controller.left || player.fixedSpeedLeft) && !player.fixedSpeedRight) {
+                //check should be player.xspeed + player.speed and an else with player.xspeed = player.maxSpeed
+                if (player.xspeed - player.speed > newMaxSpeed * -1) {
+                    player.xspeed -= player.speed;
+                }
+                else if (player.xspeed > newMaxSpeed * -1 && player.wallJumping) {
+                    player.xspeed -= player.speed;
+                }
+                else {
+                    if (player.swimming) {
+                        player.xspeed = newMaxSpeed * -1;
+                    }
+                    else {
+                        const restSpeed = player.currentMaxSpeed + player.xspeed;
+                        if (restSpeed > 0) {
+                            player.xspeed -= restSpeed;
+                        }
+                    }
+                }
+                walking = true;
+            }
+            if ((Controller.right || player.fixedSpeedRight) && !player.fixedSpeedLeft) {
+                if (player.xspeed + player.speed < newMaxSpeed) {
+                    player.xspeed += player.speed;
+                }
+                else if (player.xspeed < newMaxSpeed && player.wallJumping) {
+                    player.xspeed += player.speed;
+                }
+                else {
+                    if (player.swimming) {
+                        player.xspeed = newMaxSpeed;
+                    }
+                    else {
+                        const restSpeed = player.currentMaxSpeed - player.xspeed;
+                        if (restSpeed > 0) {
+                            player.xspeed += restSpeed;
+                        }
+                    }
+                }
+                walking = true;
+            }
+        }
+        return walking;
+    }
+
+    static slowDownWalkSpeed() {
+        const { player } = this;
+
+        if (!player.fixedSpeed) {
+            player.xspeed *= player.friction;
+            if (Math.abs(player.xspeed) < 0.5) {
+                player.xspeed = 0;
+            }
+        }
+    }
+
+    static reverseForcedRunSpeed() {
+        if (player.fixedSpeedRight) {
+            player.fixedSpeedRight = false;
+            player.fixedSpeedLeft = true;
+        }
+        else if (player.fixedSpeedLeft) {
+            player.fixedSpeedRight = true;
+            player.fixedSpeedLeft = false;
+        }
+    }
+
+    static isRunButtonReleased() {
+        const { player } = this;
+        return player.runChecked && !Controller.alternativeActionButton && (Math.abs(player.xspeed) > Math.abs(player.maxSpeed));
+    }
+}
