8e8b0428ed3b1f726baa13cf699471c73e2d048c
lobstercode <lobstersteve2@hotmail.com>
2024-04-01 20:18:42 +0200

    header design

--- HeaderNavigationHandler.js
+++ HeaderNavigationHandler.js
@@ -1,42 +1,48 @@
-function navDropDownClick() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function toolVisibilityClick(e, sectionName) {
-    const clickedElement = e.target;
-    if(e.target) {
-        const visibilityIcon = clickedElement.getElementsByTagName('img')[0];
-        const sectionElement = document.getElementById(sectionName);
-        if(visibilityIcon?.style.display == 'none') {
-            visibilityIcon.style.display = 'block';
-            sectionElement.style.display = 'inline-block';
-        }
-        else {
-            visibilityIcon.style.display = 'none';
-            sectionElement.style.display = 'none';
+function navDropDownClick(event, id = "myDropdown") {
+    event.stopPropagation();
+    const collection = document.getElementsByClassName("dropdown-content");
+    if(collection.length) {
+        for (let element of collection) {
+            if(element.id && element.id !== id) {
+                closeSpecificDropdown(element.id);
+            }
         }
     }
+    document.getElementById(id).classList.toggle("show");
 }
 
-function changeView(event) {
-    const {value} = event.target;
-    event.preventDefault();
+function changeView(value) {
     if(value === "sounds") {
         document.getElementById("gameView").style.display = "none";
         document.getElementById("soundView").style.display = "block";
+        document.getElementById("gameViewCheckmark").style.display = "none";
+        document.getElementById("soundViewCheckmark").style.display = "block";
     }
     else {
         document.getElementById("gameView").style.display = "block";
         document.getElementById("soundView").style.display = "none";
+        document.getElementById("gameViewCheckmark").style.display = "block";
+        document.getElementById("soundViewCheckmark").style.display = "none";
+    }
+}
+
+function closeSpecificDropdown(id) {
+    var myDropdown = document.getElementById(id);
+    if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
     }
 }
 
 // Close the dropdown if the user clicks outside of it
-/*window.onclick = function (e) {
+window.onclick = function (e) {
     if (!e.target.matches('.dropbtn') && !e.target.matches('.dropdown-nav-item')) {
-        var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
-            myDropdown.classList.remove('show');
+        const collection = document.getElementsByClassName("dropdown-content");
+        if(collection.length) {
+            for (let element of collection) {
+                if(element.id) {
+                    closeSpecificDropdown(element.id);
+                }
+            }
         }
     }
-}*/
+}
