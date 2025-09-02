a24baba9e5ccaf9b4e41c04bd23e9516118b31ba
l0bster <lobstersteve2@hotmail.com>
2025-09-02 22:32:40 +0200

    dialogue improvements

--- handlers/DialogueHandler.js
+++ handlers/DialogueHandler.js
@@ -39,7 +39,7 @@ class DialogueHandler {
         this.rightPos = Camera.viewport.left + Camera.viewport.width - border;
         this.topPos = Camera.viewport.top + Camera.viewport.height - (this.dialogueHeight / Camera.viewport.scale) - border;
 
-        if(this.tileMapHandler) {
+        if (this.tileMapHandler) {
             this.avatarSize = this.tileMapHandler.tileSize * 3 / Camera.viewport.scale;
             this.avatarRightPos = this.rightPos - this.avatarSize - this.avatarOuterPadding / Camera.viewport.scale
             this.avatarLeftPos = this.leftPos + this.avatarOuterPadding / Camera.viewport.scale;
@@ -92,12 +92,13 @@ class DialogueHandler {
             calculatedDialogueWidth, this.currentAnimationHeight, "000000");
         Display.drawRectangleBorder(leftPos, currentBoxTopPosition,
             calculatedDialogueWidth, this.currentAnimationHeight, "FFFFFF");
-        Display.drawLine(leftPos + calculatedDialogueWidth - 80, currentBoxTopPosition, leftPos + calculatedDialogueWidth - 20, topPos,
-            "000000", 2);
 
         const avatarOnTheLeft = this.dialogue[this.currentIndex].avatar?.position === AnimationHelper.facingDirections.left;
 
         if (this.currentAnimationHeight >= calculatedDialogueHeight) {
+            Display.drawLine(leftPos + calculatedDialogueWidth - 80, currentBoxTopPosition, leftPos + calculatedDialogueWidth - 20, topPos,
+                "000000", 2);
+
             for (var i = 0; i <= currentLine; i++) {
                 if (i < this.dialogue[this.currentIndex].lines.length) {
                     this.animateText(
@@ -131,7 +132,7 @@ class DialogueHandler {
         const avatarBorderPadding = this.avatarInnerPadding / Camera.viewport.scale;
 
         if (avatar.border) {
-            const borderSize = (this.tileMapHandler.tileSize * 3 + this.avatarInnerPadding  * 2) / Camera.viewport.scale;
+            const borderSize = (this.tileMapHandler.tileSize * 3 + this.avatarInnerPadding * 2) / Camera.viewport.scale;
             Display.drawRectangleBorder(avatarLeftPos - avatarBorderPadding,
                 top - avatarBorderPadding,
                 borderSize,
@@ -141,7 +142,7 @@ class DialogueHandler {
         Display.drawPixelArrayWithOffset(avatar.spriteObject.animation[animationIndex].sprite,
             avatarLeftPos, top,
             Math.round(this.tileMapHandler.pixelArrayUnitSize * 3 / Camera.viewport.scale),
-            avatar.spriteObject.animation[animationIndex].sprite[0].length, 
+            avatar.spriteObject.animation[animationIndex].sprite[0].length,
             avatar.spriteObject.animation[animationIndex].sprite.length
         );
     }
